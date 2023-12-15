998549be5c9c7d940c2dbe00d8012789bdeb4710
Pchev78 <pchev78@yahoo.com>
2023-12-15 11:58:58 +0100

    Affichage des données sur la map

--- script/map.js
+++ script/map.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
         maxZoom: 18,
     };
     // magnification with which the map will start
-    const zoom = 18;
+    const zoom = 12;
     // co-ordinates
-    const lat = 52.22977;
-    const lng = 21.01178;
+    const lat = 48.866667;
+    const lng = 2.333333;
 
     // calling map
     const map = L.map("map").setView([lat, lng], zoom);
@@ -18,4 +18,31 @@ $(document).ready(function () {
         attribution:
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
+
+    // Read markers data from data.csv
+    $.get('../coronavirus-commercants-parisiens-livraison-a-domicile.csv', function(csvString) {
+
+        // Use PapaParse to convert string to array of objects
+        var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+        let markers = L.markerClusterGroup();
+
+        // For each row in data, create a marker and add it to the map
+        // For each row, columns `geo_point_2d`, `Nom du commerce`, and `Adresse` are required
+        for (var i in data) {
+            var row = data[i];
+
+            var marker = L.marker(row.geo_point_2d.split(","), {
+                opacity: 1
+            }).bindPopup(row['Nom du commerce'] + "<br>" + row['Adresse']);
+
+            markers.addLayer(marker);
+        }
+        map.addLayer(markers);
+
+    });
+
+
+    map.attributionControl.setPrefix(
+        'View <a href="https://github.com/HandsOnDataViz/leaflet-map-csv" target="_blank">code on GitHub</a>'
+    );
 })

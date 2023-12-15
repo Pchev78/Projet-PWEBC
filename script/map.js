ffb444a690a8f3a0ca77cc1e1623f6dd23e813f5
Pchev78 <pchev78@yahoo.com>
2023-12-15 19:55:11 +0100

    Réglage du paramètre de zoom de l'utilisateur

--- script/map.js
+++ script/map.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // config map
     let config = {
-        minZoom: 7,
-        maxZoom: 18,
+        minZoom: 11,
+        maxZoom: 20,
     };
     // magnification with which the map will start
     const zoom = 12;
@@ -11,7 +11,7 @@ $(document).ready(function () {
     const lng = 2.333333;
 
     // calling map
-    const map = L.map("map").setView([lat, lng], zoom);
+    const map = L.map("map", config).setView([lat, lng], zoom);
     // Used to load and display tile layers on the map
     // Most tile servers require attribution, which you can set under `Layer`
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -20,29 +20,101 @@ $(document).ready(function () {
     }).addTo(map);
 
     // Read markers data from data.csv
-    $.get('../coronavirus-commercants-parisiens-livraison-a-domicile.csv', function(csvString) {
-
+    $.get('./data.csv', function (csvString) {
         // Use PapaParse to convert string to array of objects
         var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
         let markers = L.markerClusterGroup();
-
-        // For each row in data, create a marker and add it to the map
-        // For each row, columns `geo_point_2d`, `Nom du commerce`, and `Adresse` are required
-        for (var i in data) {
-            var row = data[i];
-
-            var marker = L.marker(row.geo_point_2d.split(","), {
-                opacity: 1
-            }).bindPopup(row['Nom du commerce'] + "<br>" + row['Adresse']);
-
-            markers.addLayer(marker);
+        let types = new Set(data.map(row => row['Type de commerce']));
+        for (let type of types) {
+            let typeMarkers = new L.FeatureGroup();
+            for (let row of data) {
+                if (row['Type de commerce'] === type) {
+                    let marker = L.marker(row.geo_point_2d.split(","), {
+                        opacity: 1
+                    }).bindPopup(row['Nom du commerce'] + "<br>" + row['Adresse']);
+                    typeMarkers.addLayer(marker);
+                }
+            }
+            markers.addLayer(typeMarkers);
         }
         map.addLayer(markers);
-
     });
 
 
-    map.attributionControl.setPrefix(
-        'View <a href="https://github.com/HandsOnDataViz/leaflet-map-csv" target="_blank">code on GitHub</a>'
-    );
-})
+    /*
+        // centering a group of markers
+        map.on("layeradd layerremove", function () {
+            // Create new empty bounds
+            let bounds = new L.LatLngBounds();
+            // Iterate the map's layers
+            map.eachLayer(function (layer) {
+                // Check if layer is a featuregroup
+                if (layer instanceof L.FeatureGroup) {
+                    // Extend bounds with group's bounds
+                    bounds.extend(layer.getBounds());
+                }
+            });
+
+            // Check if bounds are valid (could be empty)
+            if (bounds.isValid()) {
+                // Valid, fit bounds
+                map.flyToBounds(bounds);
+            } else {
+                // Invalid, fit world
+                // map.fitWorld();
+            }
+        });
+
+
+        L.Control.CustomButtons = L.Control.Layers.extend({
+            onAdd: function () {
+                this._initLayout();
+                this._addMarker();
+                this._removeMarker();
+                this._update();
+                return this._container;
+            },
+            _addMarker: function () {
+                this.createButton("add", "add-button");
+            },
+            _removeMarker: function () {
+                this.createButton("remove", "remove-button");
+            },
+            createButton: function (type, className) {
+                const elements = this._container.getElementsByClassName(
+                    "leaflet-control-layers-list"
+                );
+                const button = L.DomUtil.create(
+                    "button",
+                    `btn-markers ${className}`,
+                    elements[0]
+                );
+                button.textContent = `${type} markers`;
+
+                L.DomEvent.on(button, "click", function (e) {
+                    const checkbox = document.querySelectorAll(
+                        ".leaflet-control-layers-overlays input[type=checkbox]"
+                    );
+
+                    // Remove/add all layer from map when click on button
+                    [].slice.call(checkbox).map((el) => {
+                        el.checked = type === "add" ? false : true;
+                        el.click();
+                    });
+                });
+            },
+        });
+
+        new L.Control.CustomButtons(null, overlayMaps, { collapsed: false }).addTo(map);
+
+    */
+    // coordinates limiting the map
+    function getBounds() {
+        const southWest = new L.LatLng(lat - 0.001, lng - 0.001);
+        const northEast = new L.LatLng(lat + 0.001, lng + 0.001);
+        return new L.LatLngBounds(southWest, northEast);
+    }
+
+    // set maxBounds
+    map.setMaxBounds(map.getBounds());
+});

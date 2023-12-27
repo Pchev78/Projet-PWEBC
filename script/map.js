ec3e22e2425ad81860f67e9e34b603a94ac68f04
Pchev78 <pchev78@yahoo.com>
2023-12-27 16:13:25 +0100

    Améliorations diverses
    * Localisation :
      * Quand l'utilisateur est dans les limites, la carte affiche sa position
      * Lorsqu'il ne l'est pas, on affiche la position de base
    * Changement de la taille de la miniMap, qui prenait trop de place.

--- script/map.js
+++ script/map.js
@@ -22,12 +22,12 @@ $(document).ready(function () {
     var baseLayers = {
         OSM:  L.tileLayer(osmURL, {minZoom: config.minZoom, maxZoom: config.maxZoom, attribution: osmAttrib}),
         DarkMap: L.tileLayer(cartoURL, {minZoom: config.minZoom, maxZoom: config.maxZoom, attribution: cartoAttrib}),
-        Topographie: L.tileLayer(otmURL, {minZoom: config.minZoom, maxZoom: config.maxZoom, attribution: otmAttrib})
+        Routes: L.tileLayer(otmURL, {minZoom: config.minZoom, maxZoom: 20, attribution: otmAttrib})
     };
     var baseLayersMiniMap = {
         OSM:  L.tileLayer(osmURL, {minZoom: 0, maxZoom: config.maxZoom, attribution: osmAttrib}),
         DarkMap: L.tileLayer(cartoURL, {minZoom: 0, maxZoom: config.maxZoom, attribution: cartoAttrib}),
-        Topographie: L.tileLayer(otmURL, {minZoom: 0, maxZoom: config.maxZoom, attribution: otmAttrib})
+        Routes: L.tileLayer(otmURL, {minZoom: 0, maxZoom: config.maxZoom, attribution: otmAttrib})
     };
 
 
@@ -37,7 +37,13 @@ $(document).ready(function () {
     var miniMap = new L.Control.MiniMap(baseLayersMiniMap.OSM, { toggleDisplay: true }).addTo(map);
     map.on('baselayerchange', function (e) {
         miniMap.changeLayer(baseLayersMiniMap[e.name]);
-    })
+    });
+    miniMap.options.width = 120;
+    miniMap.options.height = 120;
+
+    var miniMapContainer = miniMap.getContainer();
+    miniMapContainer.style.height = "120px";
+    miniMapContainer.style.width = "120px";
 
     // Read markers data from csv
     $.get('./velib.csv', function (csvString) {
@@ -46,7 +52,6 @@ $(document).ready(function () {
         let markers = L.markerClusterGroup();
         for (let elt in data) {
             let row = data[elt];
-            console.log([row.longitude, row.latitude]);
             let marker = L.marker([row.longitude, row.latitude], {
                 opacity: 1
             }).bindPopup(row.name);
@@ -55,16 +60,12 @@ $(document).ready(function () {
         map.addLayer(markers);
     });
 
-
-
     // coordinates limiting the map
-    function getBounds() {
-        const southWest = new L.LatLng(48.754194, 2.133472);
-        const northEast = new L.LatLng(49.017367, 2.590224);
-        return new L.LatLngBounds(southWest, northEast);
-    }
+    const southWest = new L.LatLng(48.754194, 2.133472); // Coordonnées des Loges-en-Josas
+    const northEast = new L.LatLng(49.017367, 2.590224); // Coordonnées du Mesnil-Amelot
+    let bounds = new L.LatLngBounds(southWest, northEast);
 
-    map.setMaxBounds(getBounds());
+    map.setMaxBounds(bounds);
 
     // 22 - Center map
     function clickZoom(e) {
@@ -78,9 +79,13 @@ $(document).ready(function () {
             setView: true,
             enableHighAccuracy: true,
         })
-        // if location found show marker and circle
         .on("locationfound", (e) => {
-            const latUser = e.latitude, lgnUser = e.longitude; // @TODO A utiliser pour la suite
+            let latUser = e.latitude, lgnUser = e.longitude; // @TODO A utiliser pour la suite
+            let userLoc = new L.LatLng(latUser, lgnUser);
+            if (bounds.contains(userLoc))
+                map.setView(userLoc);
+            else
+                map.setView([lat, lng], zoom);
         });
 
     // 42 - Echelle
@@ -101,73 +106,4 @@ $(document).ready(function () {
         * 64 - Autocomplete with geojson
         * 69 - Simple animation of jumping marker
      */
-
-
-    /*
-        // centering a group of markers
-        map.on("layeradd layerremove", function () {
-            // Create new empty bounds
-            let bounds = new L.LatLngBounds();
-            // Iterate the map's layers
-            map.eachLayer(function (layer) {
-                // Check if layer is a featuregroup
-                if (layer instanceof L.FeatureGroup) {
-                    // Extend bounds with group's bounds
-                    bounds.extend(layer.getBounds());
-                }
-            });
-
-            // Check if bounds are valid (could be empty)
-            if (bounds.isValid()) {
-                // Valid, fit bounds
-                map.flyToBounds(bounds);
-            } else {
-                // Invalid, fit world
-                // map.fitWorld();
-            }
-        });
-
-
-        L.Control.CustomButtons = L.Control.Layers.extend({
-            onAdd: function () {
-                this._initLayout();
-                this._addMarker();
-                this._removeMarker();
-                this._update();
-                return this._container;
-            },
-            _addMarker: function () {
-                this.createButton("add", "add-button");
-            },
-            _removeMarker: function () {
-                this.createButton("remove", "remove-button");
-            },
-            createButton: function (type, className) {
-                const elements = this._container.getElementsByClassName(
-                    "leaflet-control-layers-list"
-                );
-                const button = L.DomUtil.create(
-                    "button",
-                    `btn-markers ${className}`,
-                    elements[0]
-                );
-                button.textContent = `${type} markers`;
-
-                L.DomEvent.on(button, "click", function (e) {
-                    const checkbox = document.querySelectorAll(
-                        ".leaflet-control-layers-overlays input[type=checkbox]"
-                    );
-
-                    // Remove/add all layer from map when click on button
-                    [].slice.call(checkbox).map((el) => {
-                        el.checked = type === "add" ? false : true;
-                        el.click();
-                    });
-                });
-            },
-        });
-
-        new L.Control.CustomButtons(null, overlayMaps, { collapsed: false }).addTo(map);
-
-    */
 });

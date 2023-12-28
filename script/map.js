1410e5d2fcaee50fd36f7b1c1d40ddd29cc8bb25
Pchev78 <pchev78@yahoo.com>
2023-12-28 14:54:03 +0100

    Couleur des marqueurs
    Les marqueurs ont une couleur différente en fonction de la capacité du parking.
    Suppression de données vides dans la base de données (capacity = 0)

--- script/map.js
+++ script/map.js
@@ -45,6 +45,56 @@ $(document).ready(function () {
     miniMapContainer.style.height = "120px";
     miniMapContainer.style.width = "120px";
 
+    // 13 - Différenciation des marqueurs
+    // LEGENDS
+
+    // the control element is placed in the bottom right corner
+    const legend = L.control({
+        position: "bottomleft",
+    });
+
+    // we create a div with a legend class
+    const div = L.DomUtil.create("div", "legend");
+    // color table
+    const color = ["#0068a4", "#acc8d1", "#f0c7b4", "#ff8366", "#ff2e20", "#6e0003"];
+    // table of texts that will appear in the popup and legend
+    const label = ["11-20", "21-30", "31-40", "41-50", "51-60", "61 et plus"];
+
+    // we add records to the L.control method
+    const rows = [];
+    legend.onAdd = function () {
+        color.map((item, index) => {
+            rows.push(`
+                <div class="row">
+                  <i style="background: ${item}"></i>${label[index]}
+                </div>  
+            `);
+        });
+        div.innerHTML = rows.join("");
+        return div;
+    };
+
+    // we are adding a legend to the map
+    legend.addTo(map);
+
+    function colorMarker(color) {
+        const svgTemplate = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
+          <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
+          <path stroke="#fff" fill="${color}" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
+        </svg>`;
+
+        const icon = L.divIcon({
+            className: "marker",
+            html: svgTemplate,
+            iconSize: [40, 40],
+            iconAnchor: [12, 24],
+            popupAnchor: [7, -16],
+        });
+
+        return icon;
+    }
+
     // Read markers data from csv
     $.get('./velib.csv', function (csvString) {
         // Use PapaParse to convert string to array of objects
@@ -52,8 +102,10 @@ $(document).ready(function () {
         let markers = L.markerClusterGroup();
         for (let elt in data) {
             let row = data[elt];
+            let colorCapacity = Math.floor(row.capacity/10) - 1;
             let marker = L.marker([row.longitude, row.latitude], {
-                opacity: 1
+                opacity: 1,
+                icon: colorMarker(color[colorCapacity])
             }).bindPopup(row.name);
             markers.addLayer(marker);
         }
@@ -89,11 +141,11 @@ $(document).ready(function () {
         });
 
     // 42 - Echelle
-    L.control
-        .scale({
-            imperial: false,
-        })
-        .addTo(map);
+    // L.control
+    //     .scale({
+    //         imperial: false,
+    //     })
+    //     .addTo(map);
 
         /*@TODO A implementer depuis ce site https://tomickigrzegorz.github.io/leaflet-examples/# :
         * 8/45 - Control different groups of markers / Multi layer search

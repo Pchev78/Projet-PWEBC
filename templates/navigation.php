642cd9813d8ed3de0b6e360a3f1761bd29ff5bcf
Pchev78 <pchev78@yahoo.com>
2024-01-03 22:18:02 +0100

    Utilisation du PHP pour la page d'accueil

---
+++ templates/navigation.php
@@ -0,0 +1,20 @@
+<nav class="navbar">
+    <div class="logo">
+        <img src="img/logo.png" alt="Logo">
+    </div>
+    <div class="nav-links">
+        <div class="menu">
+            <a href="#">🏠 ?</a>
+            <a href="#">📰 ?</a>
+            <a href="#">🧐 ?</a>
+            <a href="#">🌍 ?</a>
+        </div>
+        <div class="auth-links">
+            <a href="connexion.php">Se connecter</a>
+            <a href="creerCompte.php">Pas encore de compte ?</a>
+        </div>
+    </div>
+    <div id ="nomCompte">
+        <?php echo $email?>
+    </div>
+</nav>
